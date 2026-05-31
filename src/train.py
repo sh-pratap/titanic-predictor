416e07ad9937639d75fd961e8913446c23312937
Shresth Pratap <shresthpratap@gmail.com>
2026-05-31 13:30:07 +0530

    Did somethings with data

---
+++ src/train.py
@@ -0,0 +1,13 @@
+# Importing libs
+import pandas as pd
+
+# Loading the dataset
+df = pd.read_csv("dataset/titanic.csv")
+
+# Cutting the survived column and placing it at the end
+survived = df.pop("Survived")
+df["Survived"] = survived
+
+# Splitting the dataset into matrix of features and dependent variable
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
